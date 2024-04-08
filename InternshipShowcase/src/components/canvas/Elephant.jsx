da53dbad664f1a96661975071a96a8ff693804e2
Roy Bastiaansen <Roybas2001@gmail.com>
2024-04-08 10:43:05 +0200

    Rotation Elephant
    Make the elephant rotate

--- InternshipShowcase/src/components/canvas/Elephant.jsx
+++ InternshipShowcase/src/components/canvas/Elephant.jsx
@@ -63,6 +63,7 @@ const ElephantCanvas = () => {
             <Suspense fallback={<CanvasLoader />}>
               <OrbitControls 
                 enablePan
+                autoRotate
                 enableZoom={false}
                 maxPolarAngle={Math.PI / 2}
                 minPolarAngle={Math.PI / 2}
