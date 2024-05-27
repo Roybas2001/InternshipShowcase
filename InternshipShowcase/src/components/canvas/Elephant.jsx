b0b480e0ea790871fffb827e5db029e1f62ed40e
Roy Bastiaansen <Roybas2001@gmail.com>
2024-05-27 16:39:29 +0200

    -

--- InternshipShowcase/src/components/canvas/Elephant.jsx
+++ InternshipShowcase/src/components/canvas/Elephant.jsx
@@ -63,7 +63,7 @@ const ElephantCanvas = () => {
             <Suspense fallback={<CanvasLoader />}>
               <OrbitControls 
                 enablePan
-                autoRotate
+                // autoRotate
                 enableZoom={false}
                 maxPolarAngle={Math.PI / 2}
                 minPolarAngle={Math.PI / 2}
